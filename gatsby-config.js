4f02685cfce2d9211a8009c9bb812f85bcbfebb0
Ronny Freites <ronnyfreites@Ronnys-MacBook-Pro.local>
2019-12-17 16:40:42 -0600

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,86 @@
+module.exports = {
+  siteMetadata: {
+    title: `Ronny Freites`,
+    subtitle: `software developer`,
+    description: `I'm a Software Developer Engineer with wide experience writing javascript code including React, NodeJs and so many others technologies.`,
+    author: `@rfreites`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-5GJQ78R",
+  
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+  
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: "gatsby" }
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/content`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-emotion`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-reading-time`, {
+          resolve: `gatsby-remark-prismjs`,
+          options: {
+            aliases:{sh: "bash", js:"javascript"},
+            showLineNumbers: true,
+          }
+        }],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        headers: {
+          "/*": [
+            "Strict-Transport-Security: max-age=63072000"
+          ]
+        }, // option to add more headers. `Link` headers are transformed by the below criteria
+        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ gatsby-node.js
@@ -0,0 +1,77 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+
+// You can delete this file if you're not using it
+
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+// const ghpages = require('gh-pages');
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode, basePath: `pages` })
+    createNodeField({
+      node,
+      name: `slug`,
+      value: slug,
+    })
+  }
+}
+
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions
+//   const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             frontmatter {
+//               path
+//               draft
+//             }
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     if (result.errors) {
+//       return Promise.reject(result.errors)
+//     }
+//     result.data.allMarkdownRemark.edges
+//       .filter(({ node }) => !node.frontmatter.draft)
+//       .forEach(({ node }) => {
+//         createPage({
+//           path: node.frontmatter.path,
+//           component: blogPostTemplate,
+//           slug: node.fields.slug,
+//           context: {},
+//         })
+//       })
+//   })
+// }
+
+/**
+ * This task pushes to the `master` branch of the configured `repo`.
+ */
+// ghpages.publish('docs', {
+//     branch: 'master',
+//     repo: 'https://github.com/rfreites/rfreites.github.io'
+//   }, function() {
+//       console.log('deploy done');
+//   });
